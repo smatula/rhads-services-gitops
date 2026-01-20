1431ee7152ec9a84b3c12e0ad58f0164e4d24424
Stephen Matula <smatula@redhat.com>
2026-01-20 13:13:54 -0500

    Mods to single keycloakrealm import

--- bootstrap.sh
+++ bootstrap.sh
@@ -46,10 +46,12 @@ spec:
         hs = {}
         hs.status = "Progressing"
         hs.message = ""
-        if obj.status ~= nil and obj.status.health ~= nil then
-          hs.status = obj.status.health.status
-          if obj.status.health.message ~= nil then
-            hs.message = obj.status.health.message
+        if obj.status ~= nil then
+          if obj.status.health ~= nil then
+            hs.status = obj.status.health.status
+            if obj.status.health.message ~= nil then
+              hs.message = obj.status.health.message
+            end
           end
         end
         return hs
